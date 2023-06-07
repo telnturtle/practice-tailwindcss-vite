10a53882a994a2171a3739edbbde8ffc72cf08aa
Eom Hwi-yong <telnturtle@gmail.com>
2023-06-08 03:40:07 +0900

    woefawiajofijewoij

--- src/components/Google.tsx
+++ src/components/Google.tsx
@@ -32,6 +32,44 @@ export function Google() {
     }
   }, [])
 
+  const handleInputExpiryDate = useCallback<
+    React.FormEventHandler<HTMLInputElement>
+  >((ev) => {
+    const formattedExpiryDate = (ev.currentTarget.value || '').substring(0, 7)
+    const expiryDate = formattedExpiryDate.replace(/\D/g, '')
+    const isSelectionDirectionBackward =
+      ev.currentTarget.selectionDirection === 'backward'
+    let cursorPosition: number =
+      (isSelectionDirectionBackward
+        ? ev.currentTarget.selectionEnd
+        : ev.currentTarget.selectionStart) ?? 0
+
+    let reformattedCardNumber = ''
+    for (let i = 0; i < expiryDate.length; i += 4) {
+      const chunk = expiryDate.substring(i, i + 4)
+      reformattedCardNumber += chunk + ' '
+    }
+    reformattedCardNumber = reformattedCardNumber.trim()
+
+    ev.currentTarget.value = reformattedCardNumber
+
+    // '1234'에서 '5'를 입력하게 되면
+    // cursorPosition이 4에서 6으로 변화되어야 하기 때문
+    if (document.activeElement === ev.currentTarget) {
+      if (ev.currentTarget.value[cursorPosition - 1] === ' ') {
+        cursorPosition += 1
+      }
+      ev.currentTarget.setSelectionRange(cursorPosition, cursorPosition)
+    }
+  }, [])
+
+  const handleInputCVC = useCallback<React.FormEventHandler<HTMLInputElement>>(
+    (ev) => {
+      undefined
+    },
+    []
+  )
+
   return (
     <div className="flex flex-col">
       <Row>
@@ -44,6 +82,35 @@ export function Google() {
             onInput={handleInputCardNumber}
             pattern="[0-9]{4}"
             placeholder="카드번호"
+            required
+            type="tel"
+          ></input>
+        </LabelInput>
+      </Row>
+      <Row>
+        <LabelInput label={'유효기간'}>
+          <input
+            aria-label="유효기간"
+            autoComplete="cc-exp"
+            className="m-2 rounded p-2 ring-1 ring-gray-400"
+            name="expiryDate"
+            onInput={handleInputExpiryDate}
+            pattern="\d{2}\s/\s\d{2}"
+            placeholder="MM/YY"
+            required
+            type="tel"
+          ></input>
+        </LabelInput>
+        <LabelInput label={'비밀번호'}>
+          <input
+            aria-label="비밀번호"
+            autoComplete="cc-csc"
+            className="m-2 rounded p-2 ring-1 ring-gray-400"
+            name="cardNumber"
+            onInput={handleInputCardNumber}
+            pattern="[0-9]{3}"
+            placeholder="CVC"
+            required
             type="tel"
           ></input>
         </LabelInput>
