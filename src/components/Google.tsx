3d5cca077f4a8b910c37d1a6b99971657fbabea8
Eom Hwi-yong <telnturtle@gmail.com>
2023-06-07 16:37:05 +0900

    Added card number input field

---
+++ src/components/Google.tsx
@@ -0,0 +1,55 @@
+import React, { useCallback } from 'react'
+
+export function Google() {
+  const handleInputCardNumber = useCallback<
+    React.FormEventHandler<HTMLInputElement>
+  >((ev) => {
+    const formattedCardNumber = ev.currentTarget.value || ''
+    const cardNumber = formattedCardNumber.replace(/\D/g, '')
+    const isSelectionDirectionBackward =
+      ev.currentTarget.selectionDirection === 'backward'
+    let cursorPosition: number =
+      (isSelectionDirectionBackward
+        ? ev.currentTarget.selectionEnd
+        : ev.currentTarget.selectionStart) ?? 0
+
+    let reformattedCardNumber = ''
+    for (let i = 0; i < cardNumber.length; i += 4) {
+      const chunk = cardNumber.substring(i, i + 4)
+      reformattedCardNumber += chunk + ' '
+    }
+    reformattedCardNumber = reformattedCardNumber.trim()
+
+    ev.currentTarget.value = reformattedCardNumber
+
+    // '1234'에서 '5'를 입력하게 되면
+    // cursorPosition이 4에서 6으로 변화되어야 하기 때문
+    if (document.activeElement === ev.currentTarget) {
+      if (ev.currentTarget.value[cursorPosition - 1] === ' ') {
+        cursorPosition += 1
+      }
+      ev.currentTarget.setSelectionRange(cursorPosition, cursorPosition)
+    }
+  }, [])
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex">
+        <label className="flex w-full flex-col">
+          <span>카드번호</span>
+          <input
+            aria-label="카드번호"
+            autoComplete="cc-number"
+            className="m-2 rounded p-2 ring-1 ring-gray-400"
+            name="cardNumber"
+            onInput={handleInputCardNumber}
+            pattern="[0-9]{4}"
+            placeholder="카드번호"
+            type="tel"
+          ></input>
+        </label>
+      </div>
+      <div className="flex"></div>
+    </div>
+  )
+}
