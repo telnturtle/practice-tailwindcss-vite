766a5545ab9f5eec232bd7ad421a4d69b9e859ae
Eom Hwi-yong <telnturtle@gmail.com>
2023-06-08 23:55:50 +0900

    Added expiration date and password input fields

--- src/components/Google.tsx
+++ src/components/Google.tsx
@@ -23,7 +23,8 @@ export function Google() {
     ev.currentTarget.value = reformattedCardNumber
 
     // '1234'에서 '5'를 입력하게 되면
-    // cursorPosition이 4에서 6으로 변화되어야 하기 때문
+    // cursorPosition이 이렇게 변화되어야 하기 때문
+    // 입력 전: 4, 핸들러 진입: 5, 핸들러 실행 뒤: 6
     if (document.activeElement === ev.currentTarget) {
       if (ev.currentTarget.value[cursorPosition - 1] === ' ') {
         cursorPosition += 1
@@ -31,6 +32,66 @@ export function Google() {
       ev.currentTarget.setSelectionRange(cursorPosition, cursorPosition)
     }
   }, [])
+
+  const handleInputExpiryDate = useCallback<
+    React.FormEventHandler<HTMLInputElement>
+  >((ev) => {
+    const formattedExpiryDate = (ev.currentTarget.value || '').substring(0, 7)
+    let expiryDate = formattedExpiryDate.replace(/\D/g, '')
+    const isSelectionDirectionBackward =
+      ev.currentTarget.selectionDirection === 'backward'
+    let cursorPosition: number =
+      (isSelectionDirectionBackward
+        ? ev.currentTarget.selectionEnd
+        : ev.currentTarget.selectionStart) ?? 0
+
+    let reformattedExpiryDate = ''
+    if (expiryDate) {
+      if (/[2-9]/.test(expiryDate[0])) {
+        reformattedExpiryDate = '0' + expiryDate[0]
+        expiryDate = expiryDate.slice(1)
+      } else {
+        reformattedExpiryDate = expiryDate.slice(0, 2)
+        expiryDate = expiryDate.slice(2)
+      }
+    }
+    if (expiryDate) {
+      reformattedExpiryDate += ' / ' + expiryDate.slice(0, 2)
+    }
+
+    reformattedExpiryDate = reformattedExpiryDate.trim()
+
+    ev.currentTarget.value = reformattedExpiryDate
+
+    // '12'에서 '3'를 입력하게 되면
+    // cursorPosition이 이렇게 변화되어야 하기 때문
+    // 입력 전: 2, 핸들러 진입: 3, 핸들러 실행 뒤: 6
+    if (document.activeElement === ev.currentTarget) {
+      if (ev.currentTarget.value[cursorPosition - 1] === ' ') {
+        cursorPosition += 3
+      }
+      ev.currentTarget.setSelectionRange(cursorPosition, cursorPosition)
+    }
+  }, [])
+
+  const handleInputPassword = useCallback<React.FormEventHandler<HTMLInputElement>>(
+    (ev) => {
+      const formattedPassword = ev.currentTarget.value || ''
+      const password = formattedPassword.replace(/\D/g, '').substring(0, 2)
+      const cursorPosition = Math.max(0, 2 * password.length - 1)
+
+      const firstDigit = password[0] ?? ' '
+      const secondDigit = password[1] ?? ' '
+      const reformattedPassword = `${firstDigit} ${secondDigit} ﹡ ﹡`
+
+      ev.currentTarget.value = reformattedPassword
+
+      if (document.activeElement === ev.currentTarget) {
+        ev.currentTarget.setSelectionRange(cursorPosition, cursorPosition)
+      }
+    },
+    []
+  )
 
   return (
     <div className="flex flex-col">
@@ -44,6 +105,35 @@ export function Google() {
             onInput={handleInputCardNumber}
             pattern="[0-9]{4}"
             placeholder="카드번호"
+            required
+            type="tel"
+          ></input>
+        </LabelInput>
+      </Row>
+      <Row>
+        <LabelInput label={'유효기간'}>
+          <input
+            aria-label="유효기간"
+            autoComplete="cc-exp"
+            className="m-2 rounded p-2 ring-1 ring-gray-400"
+            name="expiryDate"
+            onInput={handleInputExpiryDate}
+            pattern="\d{2}\s/\s\d{2}"
+            placeholder="MM/YY"
+            required
+            type="tel"
+          ></input>
+        </LabelInput>
+        <LabelInput label={'비밀번호'}>
+          <input
+            aria-label="비밀번호"
+            autoComplete="cc-csc"
+            className="m-2 rounded p-2 ring-1 ring-gray-400"
+            defaultValue={'    ﹡ ﹡'}
+            name="cardNumber"
+            onInput={handleInputPassword}
+            placeholder="CVC"
+            required
             type="tel"
           ></input>
         </LabelInput>
